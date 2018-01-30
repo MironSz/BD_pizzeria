9d078fab79b4310e55d6f13f0dc89f79699ab2e3
miron <miron.szewczyk@autonomik.pl>
2018-01-30 16:00:49 +0100

    little push

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <fstream>
 #include <pqxx/pqxx>
 
+#define SKLADNIKI "skladnik"
+#define PIZZE "pizza"
+#DEFINE KLIENCI "klient"
 using namespace std;
 using namespace pqxx;
 
@@ -16,26 +19,50 @@ string readFile(string name) {
     t.close();
     return buffer;
 }
-bool customerExists(int customer){
-    return true;//TODO
+bool customerExists(int customer, work W){
+    result r = W.exec("SELECT * from "+KLIENCI+" where id_klienta = "+customer+";");
+    return r.begin()!=r.end();
 }
-void beginNewOrder(int customer){
+int beginNewOrder(int customer, work W){
+    W.exec("INSERT INTO ZAMOWIENIE (id_klienta,udane) VALUES ("
+           +customer+",false);");
+    result r = W.exec("SELECT id_klienta from KLIENT WHERE udane = false;");
+    return (*r.begin())[0]->as<int>;
+
     //Stworz zamowienie z udane = false
 }
 
-bool addPizzaToOrder(int pizzaId, int n){
+bool addPizzaToOrder(int pizzaId,int orderId, work W){
+    W.exec("INSERT INTO PolaczeniaPizzaZamowienie (id_zamowienia,id_pizzy,ilosc)"
+                   "VALUES ("+orderId+","+pizzaId+","+ilosc+");");
+    result r = W.exec("SELECT sprawdzSkladniki("+orderId+");");
+    //TODO sprawdz to
+    if(true){
+        W.exec("DELETE from PolaczeniePizzaZamowienie WHERE id_zamowienia;");
+    }
+    return true;
     //dodaj nowe polacznie sprawdz czy starcza, ew usun polaczenie
 }
 
-int finishOrder(){
+int finishOrder(work W){
     //tylko zmien skladniki i zrob commit
 }
 
-void ingredientsDeliver(list<pair<int,string>> deliver){
+void ingredientsDeliver(list<pair<int,string>> deliver, work W){
     //dodaj skladniki
 }
 
+void createNewIngredient(string name, work W){
+    //stworz nowy skladnik
+}
+bool ingredientExists(string name, work W){
+    result r = W.exec("SELECT * from "+SKLADNIKI+" where nazwa = "+name+";");
+    return r.begin()!=r.end();
+}
 
+void parse_input(string s, work W){
+//    list<string> parsed
+}
 
 int main(int argc, char *argv[]) {
     string  dbname = "bd";
